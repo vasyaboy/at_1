09ebe0a871f475208ac6d59426c234c4c99b5be7
listmen001 <za.ignatev@gmail.com>
2025-08-11 13:41:20 +0400

    at5

---
+++ at5.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+numbers = [64, 34, 25, 12, 22, 11, 90]
+print("Исходный список:", numbers)
+sorted_numbers = bubble_sort(numbers)
+print("Отсортированный список:", sorted_numbers)
